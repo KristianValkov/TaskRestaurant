1de1fd4a88a81a58136dce3e5efb4199ba158051
KristianValkov <120708602+KristianValkov@users.noreply.github.com>
2023-12-03 20:25:31 +0200

    Add files via upload

---
+++ Проект рестурант/Restaurant/UserControl1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            //List<UserControl1> n = new List<UserControl1>();
+            //foreach (var item in n)
+            //{
+                
+            //}
+            //n.Add(this);
+        }
+    }
+}
