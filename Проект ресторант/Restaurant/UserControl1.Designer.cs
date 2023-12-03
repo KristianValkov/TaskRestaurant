a236826557df6663de88bd324ef4d572dd936964
KristianValkov <120708602+KristianValkov@users.noreply.github.com>
2023-12-03 20:31:21 +0200

    Add files via upload

---
+++ Проект ресторант/Restaurant/UserControl1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Restaurant
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblpH = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblpS = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtpH = new System.Windows.Forms.TextBox();
+            this.lblT = new System.Windows.Forms.Label();
+            this.txtT = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtpS = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Цена- 14 лв.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Грамаж- 250 г.";
+            // 
+            // lblpH
+            // 
+            this.lblpH.AutoSize = true;
+            this.lblpH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblpH.Location = new System.Drawing.Point(41, 49);
+            this.lblpH.Name = "lblpH";
+            this.lblpH.Size = new System.Drawing.Size(127, 18);
+            this.lblpH.TabIndex = 7;
+            this.lblpH.Text = "Пилешки хапки";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Колко бройки искате?";
+            // 
+            // lblpS
+            // 
+            this.lblpS.AutoSize = true;
+            this.lblpS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblpS.Location = new System.Drawing.Point(463, 49);
+            this.lblpS.Name = "lblpS";
+            this.lblpS.Size = new System.Drawing.Size(127, 18);
+            this.lblpS.TabIndex = 13;
+            this.lblpS.Text = "Патешки сърца";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.ImageLocation = "https://www.bonapeti.bg/uploads/recipes/rec20590/recipe_image0_540x405_1687340501" +
+    "74.png";
+            this.pictureBox1.Location = new System.Drawing.Point(214, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.ErrorImage")));
+            this.pictureBox2.ImageLocation = "https://m.1001recepti.com/images/photos/recipes/size_5/pecheni-pileshki-hapki-s-m" +
+    "aioneza-i-kornfleiks-6dac60e955e2a27053b127ceb24e5da1-[100487].jpg";
+            this.pictureBox2.Location = new System.Drawing.Point(5, 83);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 154);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.ErrorImage")));
+            this.pictureBox3.ImageLocation = "https://www.mycookerycollection.com/wp-content/uploads/2021/02/duck_hearts.jpg";
+            this.pictureBox3.Location = new System.Drawing.Point(427, 83);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(199, 154);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtpH
+            // 
+            this.txtpH.Location = new System.Drawing.Point(24, 312);
+            this.txtpH.Name = "txtpH";
+            this.txtpH.Size = new System.Drawing.Size(100, 22);
+            this.txtpH.TabIndex = 19;
+            // 
+            // lblT
+            // 
+            this.lblT.AutoSize = true;
+            this.lblT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblT.Location = new System.Drawing.Point(274, 49);
+            this.lblT.Name = "lblT";
+            this.lblT.Size = new System.Drawing.Size(72, 18);
+            this.lblT.TabIndex = 22;
+            this.lblT.Text = "Таратор";
+            // 
+            // txtT
+            // 
+            this.txtT.Location = new System.Drawing.Point(246, 312);
+            this.txtT.Name = "txtT";
+            this.txtT.Size = new System.Drawing.Size(100, 22);
+            this.txtT.TabIndex = 26;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(220, 293);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(151, 16);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Колко бройки искате?";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(220, 266);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(83, 16);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Цена- 10 лв.";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(220, 240);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(96, 16);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "Грамаж- 200 г.";
+            // 
+            // txtpS
+            // 
+            this.txtpS.Location = new System.Drawing.Point(446, 312);
+            this.txtpS.Name = "txtpS";
+            this.txtpS.Size = new System.Drawing.Size(100, 22);
+            this.txtpS.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Колко бройки искате?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 16);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Цена- 12 лв.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(427, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 16);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Грамаж- 150 г.";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtpS);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtT);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lblT);
+            this.Controls.Add(this.txtpH);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblpS);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblpH);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(640, 360);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblpH;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblpS;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtpH;
+        private System.Windows.Forms.Label lblT;
+        private System.Windows.Forms.TextBox txtT;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtpS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ Проект ресторант/Restaurant/UserControl4.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Restaurant
+{
+    partial class UserControl4
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl4));
+            this.txtVod = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtU = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblU = new System.Windows.Forms.Label();
+            this.txtBerr = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblVod = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBirr = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtVod
+            // 
+            this.txtVod.Location = new System.Drawing.Point(446, 310);
+            this.txtVod.Name = "txtVod";
+            this.txtVod.Size = new System.Drawing.Size(100, 22);
+            this.txtVod.TabIndex = 48;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Колко бройки искате?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Цена- 6 лв.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(427, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 16);
+            this.label7.TabIndex = 45;
+            this.label7.Text = "100 ml";
+            // 
+            // txtU
+            // 
+            this.txtU.Location = new System.Drawing.Point(246, 310);
+            this.txtU.Name = "txtU";
+            this.txtU.Size = new System.Drawing.Size(100, 22);
+            this.txtU.TabIndex = 44;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(220, 291);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(151, 16);
+            this.label11.TabIndex = 43;
+            this.label11.Text = "Колко бройки искате?";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(220, 264);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(76, 16);
+            this.label12.TabIndex = 42;
+            this.label12.Text = "Цена- 7 лв.";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(220, 238);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(45, 16);
+            this.label13.TabIndex = 41;
+            this.label13.Text = "100 ml";
+            // 
+            // lblU
+            // 
+            this.lblU.AutoSize = true;
+            this.lblU.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblU.Location = new System.Drawing.Point(291, 47);
+            this.lblU.Name = "lblU";
+            this.lblU.Size = new System.Drawing.Size(55, 18);
+            this.lblU.TabIndex = 40;
+            this.lblU.Text = "Уиски";
+            // 
+            // txtBerr
+            // 
+            this.txtBerr.Location = new System.Drawing.Point(24, 310);
+            this.txtBerr.Name = "txtBerr";
+            this.txtBerr.Size = new System.Drawing.Size(100, 22);
+            this.txtBerr.TabIndex = 39;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.ErrorImage")));
+            this.pictureBox3.ImageLocation = "https://pingo.bg/Content/products/245/800-800.jpg";
+            this.pictureBox3.Location = new System.Drawing.Point(427, 81);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(199, 154);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 38;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.ErrorImage")));
+            this.pictureBox2.ImageLocation = "https://cdncloudcart.com/16372/products/images/67178/heineken-silver-bira-v-stakl" +
+    "ena-butilka-330-ml-image_625f3f11f31b3_800x800.jpeg?1650409255";
+            this.pictureBox2.Location = new System.Drawing.Point(5, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 154);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 37;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.ImageLocation = "https://pingo.bg/Content/products/315/800-800.jpg";
+            this.pictureBox1.Location = new System.Drawing.Point(214, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 36;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblVod
+            // 
+            this.lblVod.AutoSize = true;
+            this.lblVod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblVod.Location = new System.Drawing.Point(502, 47);
+            this.lblVod.Name = "lblVod";
+            this.lblVod.Size = new System.Drawing.Size(57, 18);
+            this.lblVod.TabIndex = 35;
+            this.lblVod.Text = "Водка";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 16);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Колко бройки искате?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Цена- 4 лв.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "250 ml";
+            // 
+            // lblBirr
+            // 
+            this.lblBirr.AutoSize = true;
+            this.lblBirr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblBirr.Location = new System.Drawing.Point(87, 47);
+            this.lblBirr.Name = "lblBirr";
+            this.lblBirr.Size = new System.Drawing.Size(46, 18);
+            this.lblBirr.TabIndex = 31;
+            this.lblBirr.Text = "Бира";
+            // 
+            // UserControl4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.txtVod);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtU);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lblU);
+            this.Controls.Add(this.txtBerr);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblVod);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblBirr);
+            this.Name = "UserControl4";
+            this.Size = new System.Drawing.Size(640, 360);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVod;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtU;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblU;
+        private System.Windows.Forms.TextBox txtBerr;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblVod;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBirr;
+    }
+}
